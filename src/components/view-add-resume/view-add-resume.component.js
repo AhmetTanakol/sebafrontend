dd48e9423d4a3446f5949c7de76d576f95d9c6ff
jesseint <jesse.int@gmail.com>
2017-06-18 22:33:02 +0200

    Make code uniform and fix some styling

--- src/components/view-add-resume/view-add-resume.component.js
+++ src/components/view-add-resume/view-add-resume.component.js
@@ -23,92 +23,92 @@ class ViewAddResumeComponent {
 
 class ViewAddResumeComponentController{
     
-		constructor ($state) {
-			this.$state = $state;
-		}
+	constructor ($state) {
+		this.$state = $state;
+	}
 
-		removeCertificate (selectedItem) {
-      var index = this.certificates.indexOf(selectedItem);
-      this.certificates.splice(index, 1);
+	removeCertificate (selectedItem) {
+		var index = this.certificates.indexOf(selectedItem);
+		this.certificates.splice(index, 1);
     }
 
-		removeEducation (selectedItem) {
-      var index = this.educations.indexOf(selectedItem);
-      this.educations.splice(index, 1);
-		}
+	removeEducation (selectedItem) {
+		var index = this.educations.indexOf(selectedItem);
+		this.educations.splice(index, 1);
+	}
 
   	removeExperience (selectedItem) {
-      var index = this.experiences.indexOf(selectedItem);
-      this.experiences.splice(index, 1);
+		var index = this.experiences.indexOf(selectedItem);
+		this.experiences.splice(index, 1);
   	}
 
   	removeSkill (selectedItem) {
-      var index = this.skills.indexOf(selectedItem);
-      this.skills.splice(index, 1);
+		var index = this.skills.indexOf(selectedItem);
+		this.skills.splice(index, 1);
   	}
 
-  	addCertificate () {
-			this.certificates.push({
-				title: this.certificate.title,
-        startDate: moment(this.certificate.startDate).format('YYYY-MM-DD'),
-        endDate: moment(this.certificate.endDate).format('YYYY-MM-DD'),
-			});
-			this.certificate = {};
-		}
+	addCertificate () {
+		this.certificates.push({
+			title: this.certificate.title,
+			startDate: moment(this.certificate.startDate).format('YYYY-MM-DD'),
+			endDate: moment(this.certificate.endDate).format('YYYY-MM-DD'),
+		});
+		this.certificate = {};
+	}
 
-		addEducation () {
-			this.educations.push({
-			  schoolName: this.education.schoolName,
-			  degree: this.education.degree,
-			  startDate: moment(this.education.startDate).format('YYYY-MM-DD'),
-			  endDate: moment(this.education.endDate).format('YYYY-MM-DD'),
-			  schoolDescription: this.education.schoolDescription
-			});
-			this.education = {};
-		}
-		
-		addExperience () {
-			this.experiences.push({
-			  company: this.experience.company,
-			  jobTitle: this.experience.jobTitle,
-			  startDate: moment(this.experience.startDate).format('YYYY-MM-DD'),
-			  endDate: moment(this.experience.endDate).format('YYYY-MM-DD'),
-			  jobDescription: this.experience.jobDescription
-			});
-			this.experience = {};
-		}
+	addEducation () {
+		this.educations.push({
+		  schoolName: this.education.schoolName,
+		  degree: this.education.degree,
+		  startDate: moment(this.education.startDate).format('YYYY-MM-DD'),
+		  endDate: moment(this.education.endDate).format('YYYY-MM-DD'),
+		  schoolDescription: this.education.schoolDescription
+		});
+		this.education = {};
+	}
+	
+	addExperience () {
+		this.experiences.push({
+		  company: this.experience.company,
+		  jobTitle: this.experience.jobTitle,
+		  startDate: moment(this.experience.startDate).format('YYYY-MM-DD'),
+		  endDate: moment(this.experience.endDate).format('YYYY-MM-DD'),
+		  jobDescription: this.experience.jobDescription
+		});
+		this.experience = {};
+	}
 
-		addSkill () {
-			this.skills.push({
-				type: this.skill.type,
-				power: this.skill.power,
-			});
-			this.skill = {};
-		}
+	addSkill () {
+		this.skills.push({
+			type: this.skill.type,
+			power: this.skill.power,
+		});
+		this.skill = {};
+	}
 
-		$onInit() {
-			this.educations = [];
-			this.experiences = [];
-      this.certificates = [];
-      this.skills = [];
+	$onInit() {
+		this.educations = [];
+		this.experiences = [];
+		this.certificates = [];
+		this.skills = [];
 
-      this.certificate = {};
-      this.education = {};
-			this.experience = {};
-      this.skill = {};
+		this.certificate = {};
+		this.education = {};
+		this.experience = {};
+		this.skill = {};
 
-			var currentDate = new Date();
-			this.maxStartDate = new Date(
-				currentDate.getFullYear(),
-				currentDate.getMonth() - 1,
-				currentDate.getDate()
-			);
-			this.maxEndDate = new Date(
-				currentDate.getFullYear(),
-				currentDate.getMonth(),
-				currentDate.getDate()
-			);
-		}
+		var currentDate = new Date();
+		this.maxStartDate = new Date(
+			currentDate.getFullYear(),
+			currentDate.getMonth() - 1,
+			currentDate.getDate()
+		);
+		this.maxEndDate = new Date(
+			currentDate.getFullYear(),
+			currentDate.getMonth(),
+			currentDate.getDate()
+		);
+	}
 	
     static get $inject () {
       return ['$state'];
