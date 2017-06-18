ab030573679e113335cb87a830ed6dc90caf67b2
blank <x.or@icloud.com>
2017-06-18 22:50:30 +0200

    rebuild main page

--- src/components/view-main/view-main.component.js
+++ src/components/view-main/view-main.component.js
@@ -16,37 +16,36 @@ class ViewMainComponent {
 
 class ViewMainComponentController {
   constructor () {
-    this.myInterval = 3000;
     this.noWrapSlides = false;
     this.active = 0;
+    this.interval =3000;
     this.slides = [
-      {
-        id: 0,
-        title: 'Title of Company',
-        text: 'some information about the company',
-        image: 'https://www.travelexcellence.com/images/movil/La_Paz_Waterfall.jpg'
-      },
-      {
-        id: 1,
-        title: 'Title of Company',
-        text: 'some information about the company',
-        image: 'http://www.parasholidays.in/blog/wp-content/uploads/2014/05/holiday-tour-packages-for-usa.jpg'
-      },
-      {
-        id: 2,
-        title: 'Title of Company',
-        text: 'some information about the company',
-        image: 'http://clickker.in/wp-content/uploads/2016/03/new-zealand-fy-8-1-Copy.jpg'
-      },
-      {
-        id: 3,
-        title: 'Title of Company',
-        text: 'some information about the company',
-        image: 'http://images.kuoni.co.uk/73/indonesia-34834203-1451484722-ImageGalleryLightbox.jpg'
-      }
-    ];
+          {
+              id: 0,
+              title: 'Title of Company',
+              text: 'some information about the company',
+              image: 'https://www.travelexcellence.com/images/movil/La_Paz_Waterfall.jpg'
+          },
+          {
+              id: 1,
+              title: 'Title of Company',
+              text: 'some information about the company',
+              image: 'http://www.parasholidays.in/blog/wp-content/uploads/2014/05/holiday-tour-packages-for-usa.jpg'
+          },
+          {
+              id: 2,
+              title: 'Title of Company',
+              text: 'some information about the company',
+              image: 'http://clickker.in/wp-content/uploads/2016/03/new-zealand-fy-8-1-Copy.jpg'
+          },
+          {
+              id: 3,
+              title: 'Title of Company',
+              text: 'some information about the company',
+              image: 'http://images.kuoni.co.uk/73/indonesia-34834203-1451484722-ImageGalleryLightbox.jpg'
+          }
+      ];
   }
-
 }
 
 export default ViewMainComponent;
