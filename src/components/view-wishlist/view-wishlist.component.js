60ad6c637d9d9e98bde8dbb24969d721a9e335ed
jesseint <jesse.int@gmail.com>
2017-07-18 22:35:36 +0200

    Fix wishlist(s) problem and view

--- src/components/view-wishlist/view-wishlist.component.js
+++ src/components/view-wishlist/view-wishlist.component.js
@@ -23,28 +23,6 @@ class ViewWishlistController {
 		this.$state = $state;
 		this.UserService = UserService;	
 		this.WishlistService = WishlistService;
-      /*this.jobs = [
-        {
-          name: 'Google',
-          description: 'Hello Google',
-          image: 'http://www.gstatic.com/webp/gallery/1.jpg'
-        },
-        {
-          name: 'Microsoft',
-          description: 'Hello Microsoft',
-          image: 'http://www.gstatic.com/webp/gallery/1.jpg'
-        },
-        {
-          name: 'Airbnb',
-          description: 'Hello Airbnb',
-          image: 'http://www.gstatic.com/webp/gallery/1.jpg'
-        },
-        {
-          name: 'Spotify',
-          description: 'Hello Spotify',
-          image: 'http://www.gstatic.com/webp/gallery/1.jpg'
-        }
-      ];*/
     }
 	
 	$onInit() {
